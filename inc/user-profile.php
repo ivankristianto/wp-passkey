c060bfcea3a08fb3c5aed348d6d3b073e0a1a25a
Ivan Kristianto <ivan@ivankristianto.com>
2023-07-17 11:44:04 +0700

    Display list of user public key credentials

--- inc/user-profile.php
+++ inc/user-profile.php
@@ -8,6 +8,9 @@ declare( strict_types = 1 );
 namespace WP\Passkey\User_Profile;
 
 use Kucrut\Vite;
+use ParagonIE\ConstantTime\Base64UrlSafe;
+use Webauthn\PublicKeyCredentialUserEntity;
+use WP_Passkey\Source_Repository;
 use WP_Screen;
 use WP_User;
 
@@ -54,26 +57,49 @@ function enqueue_scripts() {
  * @return void
  */
 function display_user_passkeys( WP_User $user ) {
+	$public_key_credential_source_repository = new Source_Repository();
+
+	$user_entity = PublicKeyCredentialUserEntity::create(
+		$user->user_login,   // Name.
+		$user->user_login,  // Use user login as the ID.
+		$user->display_name, // Display name.
+	);
+
+	$public_key_credentials = $public_key_credential_source_repository->findAllForUserEntity( $user_entity );
 	?>
-	<table class="form-table">
-		<tr>
-			<th class="two-factor-main-label">
-				<?php esc_html_e( 'Passkeys', 'wp-passkey' ); ?>
-			</th>
-			<td>
-				<table class="passkeys-list-table">
-					<thead>
-						<tr>
-							<th class="col-name" scope="col"><?php esc_html_e( 'Name', 'two-factor' ); ?></th>
-							<th class="col-name" scope="col"><?php esc_html_e( 'Fingerprint', 'two-factor' ); ?></th>
-						</tr>
-					</thead>
-					<tbody>
-				</table>
-			</td>
-		</tr>
-	</table>
-	<button type="button" class="button wp-register-passkey hide-if-no-js" aria-expanded="false"><?php esc_html_e( 'Register New Passkey', 'wp-passkey' ); ?></button>
-	<p class="wp-register-passkey--message error"></p>
+	<div class="wp-passkey-admin">
+		<h2 class="wp-passkey-admin--heading"><?php esc_html_e( 'Passkeys', 'wp-passkey' ) ?></h2>
+		<p class="description">
+			<?php esc_html_e( 'Passkeys are used to authenticate you when you log in to your account.', 'wp-passkey' ); ?>
+		</p>
+		<?php
+		// @TODO: Change to WP_List_Table.
+		?>
+		<table class="wp-list-table wp-passkey-list-table widefat fixed striped table-view-list">
+			<thead>
+				<tr>
+					<th class="col-name" scope="col"><?php esc_html_e( 'Fingerprint', 'wp-passkey' ); ?></th>
+					<th class="col-name" scope="col"><?php esc_html_e( 'Type', 'wp-passkey' ); ?></th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+				foreach ( $public_key_credentials as $public_key_credential ) :
+					?>
+					<tr>
+						<td>
+							<?php echo esc_html( Base64UrlSafe::encode( $public_key_credential->getPublicKeyCredentialDescriptor()->getId() ) ); ?>
+						</td>
+						<td>
+							<?php echo esc_html( $public_key_credential->getPublicKeyCredentialDescriptor()->getType() ); ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+		<button type="button" class="button button-secondary wp-register-new-passkey hide-if-no-js" aria-expanded="false"><?php esc_html_e( 'Register New Passkey', 'wp-passkey' ); ?></button>
+		<div class="wp-register-passkey--message"></div>
+	</div>
+
 	<?php
 }

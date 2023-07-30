fec717b8af1a1fc140fb60dfa0cc62edcbbc4275
Ivan Kristianto <ivan@ivankristianto.com>
2023-07-30 16:18:02 +0700

    Store extra data for credential source

--- inc/class-source-repository.php
+++ inc/class-source-repository.php
@@ -21,6 +21,13 @@ use WP_User;
 class Source_Repository implements PublicKeyCredentialSourceRepository {
 
 	/**
+	 * Meta key prefix.
+	 *
+	 * @var string
+	 */
+	private $meta_key = 'wp_passkey_';
+
+	/**
 	 * Find a credential source by its credential ID.
 	 *
 	 * @param string $public_key_credential_id The credential ID to find.
@@ -30,7 +37,8 @@ class Source_Repository implements PublicKeyCredentialSourceRepository {
 	public function findOneByCredentialId( string $public_key_credential_id ): ?PublicKeyCredentialSource {
 		global $wpdb;
 
-		$public_key = $wpdb->get_row( $wpdb->prepare( "SELECT meta_value FROM {$wpdb->usermeta} WHERE meta_key = %s", 'wp_passkey_' . $public_key_credential_id ) );
+		$meta_key = $this->meta_key . $public_key_credential_id;
+		$public_key = $wpdb->get_row( $wpdb->prepare( "SELECT meta_value FROM {$wpdb->usermeta} WHERE meta_key = %s", $meta_key ) );
 
 		if ( ! $public_key instanceof stdClass || ! $public_key->meta_value ) {
 			return null;
@@ -81,10 +89,7 @@ class Source_Repository implements PublicKeyCredentialSourceRepository {
 
 		return array_map(
 			function( $public_key ) {
-				$public_key_credential_source = PublicKeyCredentialSource::createFromArray( $public_key );
-				$public_key_credential_source->name = $public_key['name'] ?? '';
-				$public_key_credential_source->created = $public_key['created'] ?? '';
-				return $public_key_credential_source;
+				return PublicKeyCredentialSource::createFromArray( $public_key );
 			},
 			$public_keys
 		);
@@ -94,10 +99,11 @@ class Source_Repository implements PublicKeyCredentialSourceRepository {
 	 * Save a new credential source.
 	 *
 	 * @param PublicKeyCredentialSource $public_key_credential_source The credential source to save.
+	 * @param array $extra_data Extra data to store.
 	 * @return void
 	 * @throws Exception If the user is not found.
 	 */
-	public function saveCredentialSource( PublicKeyCredentialSource $public_key_credential_source ): void {
+	public function saveCredentialSource( PublicKeyCredentialSource $public_key_credential_source, array $extra_data = [] ): void {
 		$public_key = $public_key_credential_source->jsonSerialize();
 
 		$user_handle = Base64UrlSafe::decodeNoPadding( $public_key['userHandle'] );
@@ -107,13 +113,16 @@ class Source_Repository implements PublicKeyCredentialSourceRepository {
 			throw new Exception( 'User not found.', 400 );
 		}
 
-		// Custom data to store.
-		$public_key['name'] = $public_key['userHandle'];
-		$public_key['created'] = time();
+		// Extra data to store.
+		foreach ( $extra_data as $key => $value ) {
+			$public_key['extra'][ $key ] = $value;
+		}
 
 		// Store the public key credential source. And need to add extra slashes to escape the slashes in the JSON.
 		$public_key_json = addcslashes( wp_json_encode( $public_key, JSON_UNESCAPED_SLASHES ), '\\' );
-		update_user_meta( $user->ID, 'wp_passkey_' . $public_key['publicKeyCredentialId'], $public_key_json );
+
+		$meta_key = $this->meta_key . $public_key['publicKeyCredentialId'];
+		update_user_meta( $user->ID, $meta_key, $public_key_json );
 	}
 
 	/**
@@ -121,10 +130,7 @@ class Source_Repository implements PublicKeyCredentialSourceRepository {
 	 *
 	 * @param PublicKeyCredentialSource $public_key_credential_source The credential source to delete.
 	 * @return void
-	 * @throws InvalidArgumentException
-	 * @throws RangeException
-	 * @throws TypeError
-	 * @throws Exception
+	 * @throws Exception If the user is not found.
 	 */
 	public function deleteCredentialSource( PublicKeyCredentialSource $public_key_credential_source ): void {
 		$public_key_credential_id = Base64UrlSafe::encodeUnpadded( $public_key_credential_source->getPublicKeyCredentialId() );
@@ -136,11 +142,41 @@ class Source_Repository implements PublicKeyCredentialSourceRepository {
 			throw new Exception( 'User not found.', 404 );
 		}
 
-		$is_success = delete_user_meta( $user->ID, 'wp_passkey_' . $public_key_credential_id );
+		$meta_key = $this->meta_key . $public_key_credential_id;
+		$is_success = delete_user_meta( $user->ID, $meta_key );
 
 		if ( ! $is_success ) {
 			throw new Exception( 'Unable to delete credential source.', 500 );
 		}
+	}
+
+	/**
+	 * Get extra data for a credential source.
+	 *
+	 * @param PublicKeyCredentialSource $public_key_credential_source The credential source to get extra data for.
+	 * @return array The extra data.
+	 * @throws Exception If the user is not found.
+	 */
+	public function get_extra_data( PublicKeyCredentialSource $public_key_credential_source ): array {
+		$meta_key = $this->meta_key . Base64UrlSafe::encodeUnpadded( $public_key_credential_source->getPublicKeyCredentialId() );
+
+		$user_handle = $public_key_credential_source->getUserHandle();
+
+		$user = get_user_by( 'login', $user_handle );
+
+		if ( ! $user instanceof WP_User ) {
+			throw new Exception( 'User not found.', 404 );
+		}
+
+		$public_key = get_user_meta( $user->ID, $meta_key, true );
+
+		if ( ! $public_key ) {
+			throw new Exception( 'Credential source not found.', 404 );
+		}
+
+		$public_key = json_decode( $public_key, true );
+
+		return $public_key['extra'] ?? [];
 	}
 
 }

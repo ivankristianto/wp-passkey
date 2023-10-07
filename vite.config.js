5951f2e116ecfd7254e5e7b457402d1eb592e952
Ivan Kristianto <ivan@ivankristianto.com>
2023-10-07 14:08:30 +0700

    Fix vite plugin external

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,41 @@
+/* eslint-disable import/no-unresolved */
 // vite.config.js
+
+import { defineConfig } from 'vite';
 import create_config from '@kucrut/vite-for-wp';
-// eslint-disable-next-line import/no-unresolved
 import { wp_globals } from '@kucrut/vite-for-wp/utils';
 import external_globals from 'rollup-plugin-external-globals';
+import dev_externals from 'vite-plugin-external';
 
-export default create_config(
-	{
-		'user-profile': 'assets/src/js/user-profile.js',
-		'login': 'assets/src/js/login.js',
-	},
-	'assets/dist',
-	{
-		plugins: [ external_globals( { ...wp_globals() } ) ],
-	},
-);
+const entries = {
+	'user-profile': 'assets/src/js/user-profile.js',
+	'login': 'assets/src/js/login.js',
+};
+
+const outputDir = 'assets/dist';
+
+export default defineConfig( ( { command } ) => {
+	// Dev build.
+	if ( command === 'serve' ) {
+		return create_config( entries, outputDir, {
+			plugins: [ dev_externals( { externals: wp_globals() } ) ],
+		} );
+	}
+
+	// Production build.
+	return create_config( entries, outputDir, {
+		plugins: [
+			external_globals( {
+				...wp_globals(),
+			} ),
+		],
+		build: {
+			rollupOptions: {
+				external: [ ...Object.keys( wp_globals() ) ],
+				output: {
+					globals: wp_globals(),
+				},
+			},
+		},
+	} );
+} );

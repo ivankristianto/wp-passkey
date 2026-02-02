be7b4c35736879f301a0a0540cb26ef5e431738b
Ivan Kristianto <ivan@ivankristianto.com>
2026-02-02 09:46:34 +0700

    fix: Correct phpcs ignore sniff code and alignment
    - Use correct sniff code: UsedPropertyNotSnakeCase
    - Fix alignment with 2 spaces before equals sign

--- inc/user-profile.php
+++ inc/user-profile.php
@@ -106,8 +106,8 @@ function display_user_passkeys( WP_User $user ) {
 				endif;
 
 				foreach ( $public_key_credentials as $public_key_credential ) :
-					$extra_data = $public_key_credential_source_repository->get_extra_data( $public_key_credential );
-					$fingerprint = Base64UrlSafe::encodeUnpadded( $public_key_credential->publicKeyCredentialId ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.NotSnakeCaseMemberVar -- External library property.
+					$extra_data  = $public_key_credential_source_repository->get_extra_data( $public_key_credential );
+					$fingerprint = Base64UrlSafe::encodeUnpadded( $public_key_credential->publicKeyCredentialId ); // phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase -- External library property.
 					?>
 				<tr>
 					<td>

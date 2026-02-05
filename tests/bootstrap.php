47d1245533f59073010342b20f8485e2ab09328e
Ivan Kristianto <ivan@ivankristianto.com>
2026-02-05 20:09:17 +0700

    fix: Add missing Mockery autoloader to tests/bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -43,3 +43,6 @@ tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
+
+// Load Mockery for mocking.
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
